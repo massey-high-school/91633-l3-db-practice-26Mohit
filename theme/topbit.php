ebe30e163bd1847e0d56a475d2ef5b10cc31b463
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-07-21 10:36:48 +1200

    commit

---
+++ theme/topbit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta name="description" content="Chic Clothing">
+	<meta name="keywords" content="Chic Clothing">
+	<meta name="author" content="Miss Gottschalk (from Phil Adams' videos) and more than a little help from Dave">
+	<meta charset="UTF-8">
+	<title>Chic Clothing</title>
+	<link rel="stylesheet" href="theme/chic.css" title="style1" />
+</head>
+	
+	<body>
+<div class="content">
+
+	<div class="heading">
+		<h1 class="white">Chic Clothing</h1>
+	</div> <!-- end heading -->
+
+	<div class="navigation">
+		
+		Category links go here | 
+					
+		<a class="nav" href="admin/admin.php">Admin</a>
+		
+	</div>	<!-- end navigation -->
