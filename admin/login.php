d3da447e4aee569c37b8ffb27054d4f3ea745793
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-08-03 12:13:20 +1200

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 <form action="admin.php?page=adminlogin" method="post">
 
 <p>Username: <input name="username"/></p>
-<p>Password: <input name="password" /></p>
+<p>Password: <input name="password" type="password" /></p>
     
 <?php
 if(isset($_GET['error'])) {
@@ -9,6 +9,6 @@ if(isset($_GET['error'])) {
 }    
 ?>    
     
-<p><input type="submit" name="login" /></p>    
+<p><input type="submit" name="login" value="Log In" /></p>    
     
 </form>
