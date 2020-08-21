fce80aa0a59fb2feb7636cd58f9aa01d8f1c7da8
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-08-21 20:11:05 +1200

    commit

--- admin/adminlinks.php
+++ admin/adminlinks.php
@@ -1,8 +1,44 @@
 <h3>Categories</h3>
 
+    <form action="admin.php?page=addcategory" method="post">
+	    <input name="catName" value=""/>
+		<input type="submit" value="Add Category"/>
+	</form>	
+	
+	<br />
+	
+	</form action="admin.php?page=editcategory" method="post">
+	    <select name="oldcat">
+		
+		    <option value="">
+			    Choose...
+			</option>	
+		
+		    <?php
+			    $cat_sql="SELECT * FROM `L3_prac_category`";
+				$cat_query=mysqli_query($dbconnect, $cat_sql);
+				$cat_rs=mysqli_fetch_assoc($cat_query);
+				
+				do{
+				// value sent to query that edits the category
+				echo "<option value='";
+				echo $cat_rs['catName'];
+				echo "'>";
+				
+				// what the user sees in the drop down menu
+				echo $cat_rs['catName'];
+				echo "</option>";
+				}
+				
+				while($cat_rs=mysqli_fetch_assoc($cat_query))
+			?>
+			
+		</select>
+		&nbsp; &nbsp; Change to... <input name="newcat" value=""/>
+		<input type="submit" value="Update Category"/>
+		
+    </form>		
 <p>
-    <a href="admin.php?page=addcategory">Add a Category</a><br />
-    <a href="admin.php?page=editcategory">Edit a Category</a><br />
     <a href="admin.php?page=deletecategoryselect">Delete a Category</a><br />
 </p>
 
---
+++ admin/editcatsuccess.php
@@ -0,0 +1,7 @@
+<h1>Edit Category Success!</h1>
+
+<p>The category name has been edited.</p>
+
+<?php
+    include("adminlinks.php");
+?>	
